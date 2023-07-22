0768909ea0ddd85909251e243a42045b7b96fd3d
ElenKa <test@gmail.com>
2023-07-22 23:25:30 +0700

    task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,19 +38,18 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-//        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
 //    data.order.address = address;
-    data.order.address = address;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
 //    data.order.sum = name + phone + address + goods + sum;
     data.order.sum = sum;
-
 //    data.client = 'Иван +7(987)65-43-210';
-    data.client = name + phone;
+    data.client = name + " " + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
